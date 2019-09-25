864ef8034def3ce71d6bd6d1ac43731ca2cac6ce
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-09-25 17:42:24 -0300

    Add svg component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import  { ListOfCategories } from './components/ListOfCategories'
+import  { PhotoCard } from './components/PhotoCard'
+import  { Logo } from './components/Logo'
 import { GlobalStyles } from './GlobalStyles'
 
 export const App = () => (
   <>
     <GlobalStyles />
+    <Logo />
     <ListOfCategories />
+    <PhotoCard />
   </>
 )
---
+++ src/components/PhotoCard/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ImgWrapper, Img, Button } from './styles'
+
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1452857297128-d9c29adba80b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+
+export const PhotoCard = ({ id, likes = 0 , src = DEFAULT_IMAGE }) => {
+  return (
+    <article>
+      <a href={`/detail/${id}`}>
+        <ImgWrapper>
+          <Img src={src} />
+        </ImgWrapper>
+      </a>
+      <Button>
+        { likes } likes!
+      </Button>
+    </article>
+  )
+}
