04f3d95412eeebb40b6cd53a3258a9c62dbd3aaa
florlafuente <lafuente.florencia@gmail.com>
2019-11-06 16:49:14 -0300

    Add lazy load with intersection observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,19 +1,38 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
+import React, { useState, useEffect, useRef, Fragment } from 'react'
+import { Article,ImgWrapper, Img, Button } from './styles'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1452857297128-d9c29adba80b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0 , src = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+  useEffect(function () {
+    const observer = new window.IntersectionObserver(function(entries){
+     const { isIntersecting } = entries[0]
+     if (isIntersecting) {
+       setShow(true)
+       observer.disconnect()
+     }
+    })
+    observer.observe(element.current)
+  }, [element])
+
+
   return (
-    <article>
+    <Article ref={element}>
+    {
+      show && 
+      <Fragment>
       <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        { likes } likes!
-      </Button>
-    </article>
+      <ImgWrapper>
+        <Img src={src} />
+      </ImgWrapper>
+    </a>
+    <Button>
+      { likes } likes!
+    </Button>
+      </Fragment>
+    }
+    </Article>
   )
 }
