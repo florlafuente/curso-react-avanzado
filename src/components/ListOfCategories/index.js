4ed7177a2ea2ed95e6106d9008d6cd10bdb5d723
florlafuente <lafuente.florencia@gmail.com>
2019-11-06 15:44:17 -0300

    Add useState and useEffect hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
-import { categories } from '../../../api/db.json'
-
 export const ListOfCategories = () => {
+  // uso el hook setState para setear el estado con mochCategories como estado inicial
+  const [categories, setCategories] = useState([])
+  useEffect(function () {
+    window.fetch('https://petgram-api-flor.now.sh/categories')
+      .then(res => res.json())
+      .then(res => {
+        setCategories(res)
+      }, []) // el array vacio se agrega para que se ejecute solo la primera vez que se renderiza el componente y no se haga un loop infinito
+  })
   return (
     <List>
       { categories.map(category => <Item key={category.id}> <Category {...category} /> </Item>) }
