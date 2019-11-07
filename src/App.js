44a07418b5c418ae54c9406544f0157b78d722d9
florlafuente <lafuente.florencia@gmail.com>
2019-11-07 16:21:28 -0300

    Add photo card with query

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import  { ListOfCategories } from './components/ListOfCategories'
 import  { ListOfPhotoCards } from './containers/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 import  { Logo } from './components/Logo'
 import { GlobalStyles } from './styles/GlobalStyles'
 
-export const App = () => (
-  <>
-    <GlobalStyles />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards  categoryId={1} />
-  </>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+
+  const detailId = urlParams.get('detail')
+
+  return (
+    <>
+      <GlobalStyles />
+      <Logo />
+      {
+        detailId
+        ? <PhotoCardWithQuery  id={detailId}/>
+        : <>
+          <ListOfCategories />
+          <ListOfPhotoCards  categoryId={1} />
+        </>
+      }
+      
+    </>
+  )
+}
--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -8,4 +8,3 @@ export const ListOfPhotoCardsComponent = ({ data:  { photos = [] } } = {} ) => {
       </ul>
     )
   }
-
