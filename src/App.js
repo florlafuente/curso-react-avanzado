c73cb96dfb7e3f6b1dbd6ea27b158bfadd74bfb0
florlafuente <lafuente.florencia@gmail.com>
2019-11-21 22:03:54 -0300

    Add Favs, User & NotRegisteredUser pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 import  { Logo } from './components/Logo'
 import { Navbar } from './components/Navbar'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
-import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
+
 
 export const App = () => {
-
-
   return (
     <>
       <GlobalStyles />
@@ -20,6 +26,20 @@ export const App = () => {
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
       </Router>
+        <UserLogged>
+          {
+            ({ isAuth }) =>
+            isAuth
+            ? <Router>
+              <Favs path="/favs" />
+              <User path="/user" />
+            </Router>
+            : <Router>
+             <NotRegisteredUser path="/favs" />
+             <NotRegisteredUser path="/user" />
+            </Router>
+          }
+        </UserLogged>
       <Navbar />
     </>
   )
